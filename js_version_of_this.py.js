03670e285661a0555b43930f7646d8ddc3f6d35e
harshsaini786 <42185607+harshsaini786@users.noreply.github.com>
2021-04-18 05:30:21 +0530

    converted the code of this.py to javascript

---
+++ js_version_of_this.py.js
@@ -0,0 +1,39 @@
+/**
+*javascript version of https://github.com/python/cpython/blob/master/Lib/this.py
+*/
+
+const s = `Gur Mra bs Clguba, ol Gvz Crgref
+Ornhgvshy vf orggre guna htyl.
+Rkcyvpvg vf orggre guna vzcyvpvg.
+Fvzcyr vf orggre guna pbzcyrk.
+Pbzcyrk vf orggre guna pbzcyvpngrq.
+Syng vf orggre guna arfgrq.
+Fcnefr vf orggre guna qrafr.
+Ernqnovyvgl pbhagf.
+Fcrpvny pnfrf nera'g fcrpvny rabhtu gb oernx gur ehyrf.
+Nygubhtu cenpgvpnyvgl orngf chevgl.
+Reebef fubhyq arire cnff fvyragyl.
+Hayrff rkcyvpvgyl fvyraprq.
+Va gur snpr bs nzovthvgl, ershfr gur grzcgngvba gb thrff.
+Gurer fubhyq or bar-- naq cersrenoyl bayl bar --boivbhf jnl gb qb vg.
+Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.
+Abj vf orggre guna arire.
+Nygubhtu arire vf bsgra orggre guna *evtug* abj.
+Vs gur vzcyrzragngvba vf uneq gb rkcynva, vg'f n onq vqrn.
+Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl or n tbbq vqrn.
+Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!`
+
+const d ={}
+
+for(let c of [65, 97]){
+    for(let i = 0; i < 26; i++){
+        d[String.fromCharCode(i+c)] = String.fromCharCode((i+13) % 26 + c)
+    }
+}
+
+let result = "";
+for(let char of s){
+  result += char in d ? d[char]: char;
+}
+
+console.log(result)
